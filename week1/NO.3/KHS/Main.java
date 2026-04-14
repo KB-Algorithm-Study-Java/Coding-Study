c90a7d7d0d7c34471e97472eac50933e097a6df7
kohyunseok <7580750@naver.com>
2026-04-14 11:27:21 +0900

    1주차 고현석 문제풀이 최종

--- week1/NO.3/KHS/Main.java
+++ week1/NO.3/KHS/Main.java
@@ -1,81 +1,55 @@
 package KHS;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 보드판의 크기 입력 받기
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int[] mushroom = new int[10];
+        int sum = 0;
 
-        // NxM 크기의 보드판 2차원 문자 배열 생성
-        char[][] board = new char[N][M];
-
-        // 색깔 받기 전 앞에서 nextInt()를 써서 버퍼에 남은 엔터를 처리해주기
-        sc.nextLine();
-
-        // 보드판 색깔 입력받기
-        for (int i = 0; i < board.length; i++) {
-            // 문자열 받기
-            String color = sc.next();
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = color.charAt(j);
-            }
+        for (int i = 0; i < mushroom.length; i++) {
+            mushroom[i] = sc.nextInt();
         }
 
-        // 다시 칠해야 하는 정사각형의 개수
-        // 최솟값 갱신이 필요
-        int countB = 0;
-        int countW = 0;
-
-        for (int i = 0; i <= N-8; i++) {
-            for (int j = 0; j < M-8; j++) {
-                // B(검정)이 시작일 경우
-                for (int k = 0; k < 8; k++) {
-                    for (int m = 0; m < 8; m++) {
-                        // 검정이 아닐 경우
-                        if((k+m)%2==0) {
-                            if(board[i + k][j + m] != 'B') {
-                                countB ++;
-                            }
-                        }
-
-                        if((k+m)%2==1) {
-                            if(board[i + k][j + m] != 'W') {
-                                countB ++;
-                            }
-                        }
-                    }
+        for (int i = 0; i < mushroom.length; i++) {
+            sum += mushroom[i];
+            if(sum > 100) {
+                if(sum - 100 > 100 - (sum - mushroom[i])) {
+                    sum = sum - mushroom[i];
                 }
+                break;
             }
         }
 
-        for (int i = 0; i <= N-8; i++) {
-            for (int j = 0; j < M-8; j++) {
-                // B(검정)이 시작일 경우
-                for (int k = 0; k < 8; k++) {
-                    for (int m = 0; m < 8; m++) {
-                        // 검정이 아닐 경우
-                        if((k+m)%2==0) {
-                            if(board[i + k][j + m] != 'W') {
-                                countW ++;
-                            }
-                        }
-
-                        if((k+m)%2==1) {
-                            if(board[i + k][j + m] != 'B') {
-                                countW ++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        int result = (countB > countW) ? countW : countB;
-
-        System.out.println(result);
+        System.out.println(sum);
     }
 }
+
+
+// 풀이 1.
+//        Scanner sc = new Scanner(System.in);
+//
+//        int[] mushroom = new int[10];
+//        for (int i = 0; i < mushroom.length; i++) {
+//            mushroom[i] = sc.nextInt();
+//        }
+//
+//        int cA = 0;
+//        int cB = 0;
+//
+//            for (int i = 0; i < mushroom.length; i++) {
+//                cA += mushroom[i];
+//                cB += mushroom[i];
+//                if (cA > 100) {
+//                    cB -= mushroom[i];
+//                    break;
+//                }
+//            }
+//       int diffA = Math.abs(100-cA);
+//       int diffB = Math.abs(100-cB);
+//
+//       int result = (diffA == diffB) ? cA : (diffA < diffB) ? cA : cB;
+//
+//        System.out.println(result);
